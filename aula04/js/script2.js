23f9292305856806436e1e37314bd0236944cd46
FLAVIO DOS SANTOS SILVA <flavio.ssilva16@senacsp.edu.br>
2023-10-02 08:05:38 -0300

    atualizado

---
+++ aula04/js/script2.js
@@ -0,0 +1,31 @@
+// vamos fazer uma referencia a classe Date
+
+let tempo = new Date()
+console.log(tempo.getDay());
+console.log(tempo.getDate());
+console.log(tempo.getMonth());
+
+switch(tempo.getDay()){
+    case 0:
+        console.log("Domingo");
+        break
+    case 1:
+        console.log("Segunda-Feira");
+        break
+        
+    case 2: console.log("Terça-feira");
+        break
+        
+    case 3: console.log("Quarta=Feira");
+        break    
+
+    case 4: console.log("Quinta-Feira");
+        break
+        
+   case 5: console.log("Sexta-Feira");
+        break    
+    default:
+        console.log("Sabado");  
+       break 
+}       
+    
